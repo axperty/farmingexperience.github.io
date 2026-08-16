7ab26358a89c26aea64bdedac1f808ee46cad402
axperty <107956734+axperty@users.noreply.github.com>
2026-08-15 23:49:33 -0300

    Fixed posts.data.mjs.

--- posts.data.mjs
+++ posts.data.mjs
@@ -1,6 +1,6 @@
 import { createContentLoader } from 'vitepress'
 
-export default createContentLoader('posts/*-*.md', {
+export default createContentLoader('farmingexperience/posts/*-*.md', {
   transform(raw) {
     return raw
       .map(({ url, frontmatter, excerpt }) => ({
